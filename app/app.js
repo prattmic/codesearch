ace4466d6b8c6c34436c8656a0b3385695623523
Michael Pratt <michael@pratt.im>
2015-10-04 10:30:40 -0700

    Add a controller with the file to display
    We don't load it yet.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,7 @@
 (function() {
 	var app = angular.module('search', ['prettify']);
+
+	app.controller('MainController', function() {
+		this.file = "github.com/prattmic/codesearch/cmd/search/search.go";
+	});
 })();
