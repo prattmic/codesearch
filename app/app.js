cc2c65658c017699aece84e4c667aaa88b82ebaa
Michael Pratt <michael@pratt.im>
2015-10-04 10:32:30 -0700

    2 space tabs
    I'll try to follow Google's code style

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function() {
-	var app = angular.module('search', ['prettify']);
+  var app = angular.module('search', ['prettify']);
 
-	app.controller('MainController', function() {
-		this.file = "github.com/prattmic/codesearch/cmd/search/search.go";
-	});
+  app.controller('MainController', function() {
+    this.file = "github.com/prattmic/codesearch/cmd/search/search.go";
+  });
 })();
--- app/prettify.js
+++ app/prettify.js
@@ -1,22 +1,22 @@
 (function() {
-	var app = angular.module('prettify', [ ]);
+  var app = angular.module('prettify', [ ]);
 
-	app.directive('prettify', function() {
-		return {
-			restrict: 'E',
-			transclude: true,
-			templateUrl: "prettify.html",
-			link: function($scope, element, attrs) {
-				$scope.$watch(function() {
-					// Watch for the raw content to change.
-					raw = element.find("span");
-					return raw.html();
-				}, function(html){
-					// ... then redo the formatted content.
-					formatted = element.find("span").next();
-					formatted.html(prettyPrintOne(html));
-				});
-			}
-		};
-	});
+  app.directive('prettify', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      templateUrl: "prettify.html",
+      link: function($scope, element, attrs) {
+        $scope.$watch(function() {
+          // Watch for the raw content to change.
+          raw = element.find("span");
+          return raw.html();
+        }, function(html){
+          // ... then redo the formatted content.
+          formatted = element.find("span").next();
+          formatted.html(prettyPrintOne(html));
+        });
+      }
+    };
+  });
 })();
