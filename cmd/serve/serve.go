e2a03ba34d55c292ef2bb89cd8d1f0e23735d6e1
Michael Pratt <michael@pratt.im>
2015-10-04 10:39:52 -0700

    serve: serve frontend and raw source code

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -2,52 +2,26 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"path"
 
 	"github.com/gorilla/mux"
 )
 
-var root = flag.String("root", "", "Root directory from which to serve files")
-
-// SourceHandler implements http.Handler, serving the file specified by the URL.
-// If served with a prefix, use http.StripPrefix on this Handler.
-type SourceHandler struct{}
-
-func (s *SourceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p := path.Clean(r.URL.Path)
-	log.Printf("File requested: %s", p)
-
-	location := path.Join(*root, p)
-	log.Printf("Serving: %s", location)
-
-	d, err := ioutil.ReadFile(location)
-	if err != nil {
-		log.Printf("Error opening file: %v", err)
-		http.Error(w, "404 Not Found", 404)
-		return
-	}
-
-	if err := sourceTemplate.Execute(w, d); err != nil {
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "500 Internal Server Error", 500)
-		return
-	}
-}
+var (
+	app = flag.String("app", "", "Root directory from which to serve the frontend app")
+	src = flag.String("src", "", "Root directory from which to serve raw source code")
+)
 
 func main() {
 	flag.Parse()
 
 	r := mux.NewRouter()
 
-	r.Path("/").Methods("GET").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "ok")
-	})
+	r.PathPrefix("/src").Methods("GET").
+		Handler(http.StripPrefix("/src", http.FileServer(http.Dir(*src))))
 
-	r.PathPrefix("/src").Methods("GET").Handler(http.StripPrefix("/src", &SourceHandler{}))
+	r.PathPrefix("/").Methods("GET").
+		Handler(http.FileServer(http.Dir(*app)))
 
 	http.ListenAndServe(":8080", r)
 }
