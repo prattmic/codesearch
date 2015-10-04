4fca63a17841a6bc4bbd743b7540408a16be2e9d
Michael Pratt <michael@pratt.im>
2015-10-04 11:06:29 -0700

    app: load new files as specified
    A file can be typed in the search box and it will be loaded.

--- app/app.js
+++ app/app.js
@@ -1,17 +1,34 @@
 (function() {
   var app = angular.module('search', ['prettify']);
 
-  app.controller('MainController', ['$http', function($http) {
+  app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     var main = this;
     main.file = 'github.com/prattmic/codesearch/cmd/search/search.go';
     main.fileContents = "";
 
-    $http.get('/src/' + main.file)
-      .success(function(response) {
-        main.fileContents = response;
-      })
-      .error(function(response) {
-        main.fileContents = 'Unable to load ' + main.file + ': ' + response;
-      });
+    // Watch for file to change, updating contents when it does.
+    $scope.$watch(function(){
+      return main.file;
+    }, function(file) {
+      main.fileContents = "Loading...";
+
+      $http.get('/src/' + file)
+        .success(function(response) {
+          main.fileContents = response;
+        })
+        .error(function(response) {
+          main.fileContents = 'Unable to load ' + main.file + ': ' + response;
+        });
+    });
+  }]);
+
+  app.controller('SearchController', ['$http', function($http) {
+    var search = this;
+    search.query = "";
+
+    search.loadFile = function(main) {
+      main.file = search.query;
+      search.query = "";
+    };
   }]);
 })();
