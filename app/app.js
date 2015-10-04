ca296ae8a2692ebe67f64d0bf92b180d484ccc2c
Michael Pratt <michael@pratt.im>
2015-10-04 01:11:52 -0700

    prettify directive
    Wrapping text in this directive gives it nice syntax highlighting.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,21 @@
 (function() {
 	var app = angular.module('search', [ ]);
+
+	app.directive('prettify', function() {
+		return {
+			restrict: 'E',
+			transclude: true,
+			templateUrl: "prettify.html",
+			link: function($scope, element, attrs) {
+				$scope.$watch(function() {
+					raw = element.find("span");
+					return raw.html();
+				}, function(html){
+					formatted = element.find("span").next();
+
+					formatted.html(prettyPrintOne(html));
+				});
+			}
+		};
+	});
 })();
