971596e06fd2c61c8af6cb8301b6737194eac889
Michael Pratt <michael@pratt.im>
2015-10-04 10:46:35 -0700

    app: use $http service to dynamically load file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,17 @@
 (function() {
   var app = angular.module('search', ['prettify']);
 
-  app.controller('MainController', function() {
-    this.file = "github.com/prattmic/codesearch/cmd/search/search.go";
-  });
+  app.controller('MainController', ['$http', function($http) {
+    var main = this;
+    main.file = 'github.com/prattmic/codesearch/cmd/search/search.go';
+    main.fileContents = "";
+
+    $http.get('/src/' + main.file)
+      .success(function(response) {
+        main.fileContents = response;
+      })
+      .error(function(response) {
+        main.fileContents = 'Unable to load ' + main.file + ': ' + response;
+      });
+  }]);
 })();
