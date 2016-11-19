71838cd6926320b7a2d296ed0aaeff8c25e874b0
Michael Pratt <michael@pratt.im>
2016-11-19 13:40:50 -0800

    pkg/grep: add more test cases

--- pkg/grep/grep_test.go
+++ pkg/grep/grep_test.go
@@ -2,28 +2,67 @@ package grep
 
 import (
 	"bytes"
+	"io"
 	"reflect"
 	"regexp"
 	"testing"
 )
 
 func TestSearch(t *testing.T) {
-	b := bytes.NewBufferString("hello\nworld")
-	g, err := New(b)
-	if err != nil {
-		t.Fatalf("New got err %v want nil", err)
-	}
-
-	r := regexp.MustCompile("world")
-	m := g.Search(r)
-	expect := []Match{
+	testCases := []struct {
+		name string
+		in   io.Reader
+		r    *regexp.Regexp
+		e    []Match
+	}{
 		{
-			LineNum:  2,
-			FullLine: []byte("world"),
-			Match:    []byte("world"),
+			name: "simple",
+			in:   bytes.NewBufferString("hello\nworld"),
+			r:    regexp.MustCompile("world"),
+			e: []Match{
+				{
+					LineNum:  2,
+					FullLine: []byte("world"),
+					Match:    []byte("world"),
+				},
+			},
+		},
+		{
+			name: "no-matches",
+			in:   bytes.NewBufferString("hello\nworld"),
+			r:    regexp.MustCompile("foo"),
+			e:    []Match{},
+		},
+		{
+			name: "multi-match",
+			in:   bytes.NewBufferString("foo\nbar\nfabulous"),
+			r:    regexp.MustCompile("^f"),
+			e: []Match{
+				{
+					LineNum:  1,
+					FullLine: []byte("foo"),
+					Match:    []byte("f"),
+				},
+				{
+					LineNum:  3,
+					FullLine: []byte("fabulous"),
+					Match:    []byte("f"),
+				},
+			},
 		},
 	}
-	if !reflect.DeepEqual(m, expect) {
-		t.Errorf("Search got %+v want %+v", m, expect)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g, err := New(tc.in)
+			if err != nil {
+				t.Fatalf("New got err %v want nil", err)
+			}
+
+			m := g.Search(tc.r)
+			if !reflect.DeepEqual(m, tc.e) {
+				t.Errorf("Search got %+v want %+v", m, tc.e)
+			}
+		})
 	}
 }
